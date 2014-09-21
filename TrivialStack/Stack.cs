fcee64d3892033c008fc9c9958d145eb06a43b98
Chaney Fonseca <chaney.fonseca@bazaarvoice.com>
2014-09-20 19:07:11 -0500

    checking in green-red-green unit test example

---
+++ TrivialStack/Stack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrivialStack
+{
+    /*
+     * MyStack<T>
+ - Push(element : T)
+ - Pop() : T
+ - Peek() : T
+ - Count : int
+ - IsEmpty : boolean
+     */
+    public class MyStack<T> where T : new()
+    {
+        private readonly Stack<T> _internalStack;
+        
+        public MyStack()
+        {
+            _internalStack = new Stack<T>();
+        }
+
+        /// <summary>
+        /// Given an item of type T, push T onto the end of MyStack
+        /// </summary>
+        public void Push(T arg)
+        {
+            _internalStack.Push(arg);
+        }
+
+        /// <summary>
+        /// Return the first item inside MyStack and remove it from the internal store
+        /// </summary>
+        public T Pop()
+        {
+            return new T();
+        }
+
+        /// <summary>
+        /// Return the first item inside MyStack (without mutating this object)
+        /// </summary>
+        public T Peek()
+        {
+            return _internalStack.Peek();
+        }
+
+        /// <summary>
+        /// Return the number of items from the internal store of items
+        /// </summary>
+        public int Count()
+        {
+            return _internalStack.Count;
+        }
+
+        /// <summary>
+        /// Return true is the internal store contains any items and false otherwise
+        /// </summary>
+        public bool IsEmpty()
+        {
+            return true;
+        }
+    }
+}
---
+++ TrivialTDD/MyStackTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrivialStack;
+
+namespace TrivialTDD.Tests
+{
+    /*
+     * Stack<T>
+ - Push(element : T)
+ - Pop() : T
+ - Peek() : T
+ - Count : int
+ - IsEmpty : boolean
+     */
+
+    [TestClass]
+    public class MyStackTests
+    {
+        private MyStack<int> GetFreshStack()
+        {
+            return new MyStack<int>();
+        }
+
+        [TestMethod]
+        public void Pushing_One_Onto_New_Stack_Should_Yield_A_Stack_Where_First_Element_Is_One()
+        {
+            // Arrange
+            var s = GetFreshStack();
+            const int firstItemValue = 1;
+
+            // Act
+            var initialCount = s.Count();
+            s.Push(firstItemValue);
+            var firstItem = s.Peek();
+
+            // Assert
+            Assert.IsTrue(initialCount == 0, "Expected initial count of stack to be 0 but instead found value was " + initialCount);
+            Assert.IsTrue(firstItem == firstItemValue,
+                "First item in stack equals " + firstItem + " but expected " + firstItemValue);
+        }
+
+        [TestMethod]
+        public void TestPop()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestPeek()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestIsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
